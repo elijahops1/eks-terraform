5f2e083b7c9f917ac4dc63e51be87845aea49ecd
elijahops1 <141283145+elijahops1@users.noreply.github.com>
2023-08-06 11:26:31 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIARHVQYNOLLJ2VTDIU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 terraform {
