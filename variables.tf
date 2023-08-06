7d2967d35467fa695fafacc49debef28592fc86c
elijahops1 <141283145+elijahops1@users.noreply.github.com>
2023-08-06 14:24:47 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "profile" {
+  description = "Set this variable if you use another profile besides the default awscli profile called 'default'."
+  type        = string
+  default     = "jamfuser"
+}
